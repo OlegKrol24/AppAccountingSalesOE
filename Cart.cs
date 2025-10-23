bd6378ea00450d9bccc60d95e4843bae41a5004d
Your Name <you@example.com>
2025-10-23 16:56:57 +0300

    Class 'Cart' updated (checking)

--- Cart.cs
+++ Cart.cs
@@ -8,5 +8,26 @@ namespace AppAccountingSalesOE
 {
     public class Cart
     {
+        public List<Goods> GoodsInCart { get; set; }
+
+        public Cart()
+        {
+            GoodsInCart = new List<Goods>();
+        }
+
+        public decimal GetTotalPrice()
+        {
+            return GoodsInCart.Sum(g => g.Price);
+        }
+
+        public void AddToCart(Goods item)
+        {
+            GoodsInCart.Add(item);
+        }
+
+        public void RemoveFromCart(Goods item)
+        {
+            GoodsInCart.Remove(item);
+        }
     }
 }
